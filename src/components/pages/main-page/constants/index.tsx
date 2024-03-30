9540fbca74d8d5e8cf5b8b9b666ae945cba0cde9
delver <dima.plugaturevchik@gmail.com>
2024-03-30 19:32:25 +0200

    created main page

---
+++ src/components/pages/main-page/constants/index.tsx
@@ -0,0 +1,17 @@
+export const registryFeatures = [
+  {
+    title: 'Upload',
+    description: 'Upload your own archive with libraries',
+    link: '/upload',
+  },
+  {
+    title: 'Libraries',
+    description: 'Check out the libraries that are already uploaded',
+    link: '/libraries',
+  },
+  {
+    title: 'Statistics',
+    description: 'Check out the statistics of the uploaded libraries',
+    link: '/statistics',
+  },
+];
