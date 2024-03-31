7ccddf301e3473588b7692b28f6550d9225cb780
delver <dima.plugaturevchik@gmail.com>
2024-03-31 05:55:27 +0300

    improved About page text styling

--- src/components/pages/about-page/AboutPage.tsx
+++ src/components/pages/about-page/AboutPage.tsx
@@ -2,8 +2,8 @@ import { Box, Typography } from '@mui/material';
 
 const AboutPage = () => {
   return (
-    <Box>
-      <Typography variant={'h6'} mt={'50px'}>
+    <Box sx={{ mx: '10px' }}>
+      <Typography variant={'h6'} mt={'50px'} textAlign={'justify'}>
         <b>AkaShi</b> — it's an internet registry of program libraries for C#
         programming language. It's my <b>Kursach</b> at KPI.
       </Typography>
