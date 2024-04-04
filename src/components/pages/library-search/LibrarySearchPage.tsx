b7c426f2bddd04d5de119bfa2b9377d7c9da89e7
delver <dima.plugaturevchik@gmail.com>
2024-04-04 18:31:00 +0300

    extracted LibraryCard component from LibrarySearchPage

--- src/components/pages/library-search/LibrarySearchPage.tsx
+++ src/components/pages/library-search/LibrarySearchPage.tsx
@@ -1,12 +1,8 @@
 import * as React from 'react';
 import { FC } from 'react';
-import { Box, Card, CardContent, ListItem, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import Container from '@mui/material/Container';
 import { useTheme } from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { Breakpoint } from '@mui/system';
-import Image from 'next/image';
-import Link from 'next/link';
 import { usePathname, useSearchParams } from 'next/navigation';
 import { useRouter } from 'next/router';
 
@@ -14,11 +10,11 @@ import Paginator from '@/components/common/ui/paginator/Paginator';
 import FrameworkFilter, {
   FilterFrameworkProductNames,
 } from '@/components/pages/library-search/components/framework-filter/FrameworkFilter';
+import LibraryCard from '@/components/pages/library-search/components/library-card';
 import Searchbar from '@/components/pages/library-search/components/searchbar/Searchbar';
 import SortDropdown, {
   SortBy,
 } from '@/components/pages/library-search/components/sort-dropdown/SortDropdown';
-import getLatestLibraryVersion from '@/lib/utils/getLatestLibraryVersion';
 import { LibraryWithDetails } from '@/types/library';
 
 import * as styles from './LibrarySearchPage.styles';
@@ -36,9 +32,6 @@ const LibrarySearchPage: FC<LibrarySearchPageProps> = ({
   libraries,
   totalCount,
 }) => {
-  const theme = useTheme();
-  const isDesktop = useMediaQuery(theme.breakpoints.down('lg' as Breakpoint));
-
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const router = useRouter();
@@ -123,56 +116,7 @@ const LibrarySearchPage: FC<LibrarySearchPageProps> = ({
 
       <Box mb="11px">
         {libraries.map((library, index) => (
-          <ListItem key={index} divider>
-            <Card variant="outlined" sx={{ width: '100%' }}>
-              <CardContent
-                sx={{ display: 'flex', flexDirection: 'row', gap: '10px' }}
-              >
-                {!isDesktop && (
-                  <Image
-                    src={library.logoURL}
-                    alt="library logo"
-                    width={50}
-                    height={50}
-                  />
-                )}
-                <Box>
-                  <Box display="flex" columnGap="10px" flexWrap="wrap">
-                    <Link href={`/libraries/${library.id}`}>
-                      <Typography variant="h6">
-                        {library.name.replace(/\./g, '.\u200B')}
-                      </Typography>
-                    </Link>
-                    <Typography color="textSecondary">
-                      dev: {library.user.username}
-                    </Typography>
-                  </Box>
-                  <Box display="flex" columnGap="20px" flexWrap="wrap">
-                    <Typography>
-                      {library.downloadsCount.toLocaleString()} total downloads
-                    </Typography>
-                    <Typography>
-                      last updated{' '}
-                      {library.lastUpdateTime.toString().split('T')[0]}
-                    </Typography>
-                    <Typography>
-                      Latest version:{' '}
-                      {getLatestLibraryVersion(library.libraryVersions)?.name ??
-                        'N/A'}
-                    </Typography>
-                  </Box>
-                  {library.tags && (
-                    <Typography color="textSecondary">
-                      tags: {library.tags}
-                    </Typography>
-                  )}
-                  <Typography color="textSecondary">
-                    {library.shortDescription}
-                  </Typography>
-                </Box>
-              </CardContent>
-            </Card>
-          </ListItem>
+          <LibraryCard library={library} key={index} />
         ))}
       </Box>
       <Paginator
